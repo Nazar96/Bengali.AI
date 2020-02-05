a480120351b19199c57b045aa35f0da28d90ed38
Nazarii <ntehta@gmail.com>
2020-02-05 16:52:48 +0300

    Shebang fix

--- ssh_colab.sh
+++ ssh_colab.sh
@@ -1,11 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 
-#Generate root password
-import random, string, subprocess
-password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
 
 #Download ngrok
-bashCommand = """
 wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -qq -n ngrok-stable-linux-amd64.zip
 apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
@@ -15,28 +11,3 @@ echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
 echo "export LD_LIBRARY_PATH" >> /root/.bashrc
-"""
-
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-
-#Run sshd
-get_ipython().system_raw('/usr/sbin/sshd -D &')
-
-#Ask token
-print("Copy authtoken from https://dashboard.ngrok.com/auth")
-import getpass
-authtoken = getpass.getpass()
-
-#Create tunnel
-get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
-
-#Print root password
-print("Root password: {}".format(password))
-
-#Get public address
-bashCommand = """
-curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
-"""
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
